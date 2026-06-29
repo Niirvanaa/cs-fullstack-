f6d215720edc8d956a9481082d267ee902d86b78
Najah Thompson <najah.thompson@snhu.edu>
2026-06-28 23:24:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
+require('dotenv').config(); 
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//Bring in the database
+require('./app_api/models/db');
+
+// Wire in our authentication module 
+var passport = require('passport'); 
+require('./app_api/config/passport.js');
+
+// Define routers
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
+var handlebars = require('hbs');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// registers handlebars//
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -19,8 +37,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Enable CORS
+app.use('/api',(req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+  next();
+});
+
+// wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel' , travelRouter);
+app.use('/api' , apiRouter);
+
+// Catch unauthorized error and create 401 
+app.use((err, req, res, next) => { 
+  if(err.name === 'UnauthorizedError') { 
+    res 
+      .status(401) 
+      .json({"message": err.name + ": " + err.message}); 
+  } 
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
